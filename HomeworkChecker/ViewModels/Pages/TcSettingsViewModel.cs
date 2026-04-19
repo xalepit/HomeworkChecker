a7b897503cd1c5fd467f2eb882163ec92bf8a835
Xalepit <2452357375@qq.com>
2026-04-19 20:30:07 +0800

    布置好了文本比较参数页面(TcSettingsPage)的前端样式，增添占位成员（待完善） 重新选择了文本比较参数设置项和各个页面的Icon

--- HomeworkChecker/ViewModels/Pages/TcSettingsViewModel.cs
+++ HomeworkChecker/ViewModels/Pages/TcSettingsViewModel.cs
@@ -8,6 +8,21 @@ namespace HomeworkChecker.UI.ViewModels.Pages
     public partial class TcSettingsViewModel : ObservableObject, INavigationAware
     {
         private bool _isInitialized = false;
+
+        [ObservableProperty]
+        private string _trimMode; //占位
+        [ObservableProperty]
+        private int _lineSkipText; //占位
+        [ObservableProperty]
+        private int _lineOffsetText; //占位
+        [ObservableProperty]
+        private bool _ignoreBlank; //占位
+        [ObservableProperty]
+        private bool _crCrLfNotEqual; //占位
+        [ObservableProperty]
+        private int maxDiffText; //占位
+        [ObservableProperty]
+        private int maxLineText; //占位
         public Task OnNavigatedToAsync()
         {
             if (_isInitialized)
--- HomeworkChecker/ViewModels/Windows/MainWindowViewModel.cs
+++ HomeworkChecker/ViewModels/Windows/MainWindowViewModel.cs
@@ -14,19 +14,19 @@ namespace HomeworkChecker.UI.ViewModels.Windows
             new NavigationViewItem()
             {
                 Content = "Home",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.AppsList24 },
+                Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
                 TargetPageType = typeof(Views.Pages.HomePage)
             },
             new NavigationViewItem()
             {
                 Content = "TestData",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.AppsList24 },
+                Icon = new SymbolIcon { Symbol = SymbolRegular.ClipboardTextEdit24 },
                 TargetPageType = typeof(Views.Pages.TestDataPage)
             },
             new NavigationViewItem()
             {
                 Content = "txt_compare settings",
-                Icon = new SymbolIcon { Symbol = SymbolRegular.AppsList24 },
+                Icon = new SymbolIcon { Symbol = SymbolRegular.TextBoxSettings24 },
                 TargetPageType = typeof(Views.Pages.TcSettingsPage)
             }
         };
